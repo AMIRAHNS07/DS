544752e611cdd8f878bc84910d4d10d7e6882c66
AMIRAH NAJIHAH BINTI SAEDUN <77818@siswa.unimas.my>
2024-12-20 21:22:53 +0800

    Update CrackingServerInterface.java

--- CrackingServerInterface.java
+++ CrackingServerInterface.java
@@ -5,6 +5,9 @@ public interface CrackingServerInterface extends Remote {
     void startSearch(String targetHash, int passwordLength, char startChar, char endChar, int threadCount) throws RemoteException;
     boolean isPasswordFound() throws RemoteException;
     String getFoundPassword() throws RemoteException;
-    long getSearchTime() throws RemoteException;
+    long getSearchTimeInMillis() throws RemoteException;
+    String getStartTimestamp() throws RemoteException;
+    String getEndTimestamp() throws RemoteException;
     double getProgress() throws RemoteException;
+    int getThreadID() throws RemoteException;
 }
